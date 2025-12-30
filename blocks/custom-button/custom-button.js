3f578c80730cdcc6b8504a6e16fc47615141aa4a
Prathyusha <prapa@siteimprove.com>
2025-12-30 15:50:19 +0530

    #Analytics script and block changes

---
+++ blocks/custom-button/custom-button.js
@@ -0,0 +1,27 @@
+export default function decorate(block) {
+  const button = document.querySelector('.custom-button .button.secondary');
+  window.adobeDataLayer = window.adobeDataLayer || [];
+  if (button) {
+    const LINK_CLICK_EVENT_NAME = 'custom_button_click';
+    const internalCampaignId = '';
+    button.addEventListener('click', () => {
+    // datalayer push event
+      const dataLayerObject = {
+        event: LINK_CLICK_EVENT_NAME,
+        eventInfo: {
+          block_type: 'custom-button',
+          url: button.href,
+          cta_text: button.textContent,
+          internal_campaign_id: internalCampaignId,
+        },
+      };
+
+      if (window.adobeDataLayer && typeof window.adobeDataLayer.push === 'function') {
+        window.adobeDataLayer.push(dataLayerObject);
+      }
+      _satellite.track(dataLayerObject.event, adobeDataLayer);
+    });
+  }
+
+  block.append(button);
+}
