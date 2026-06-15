cd44828f7ffcf90bf61fddfd98c8e7ac1309381b
Manav Muramshetty <manav_muramshetty@epam.com>
2026-06-15 19:31:50 +0530

    updated xf logic

--- blocks/royal-footer/royal-footer.js
+++ blocks/royal-footer/royal-footer.js
@@ -1,14 +1,18 @@
+import { loadFragment } from '../fragment/fragment.js';
+
 export default async function decorate(block) {
   try {
     const xfPath = '/content/experience-fragments/aem-cloud-poc/us/en/site/footer-xf/master';
-    const resp = await fetch(`${xfPath}.plain.html`);
-    console.log('Fetching royal footer content from EXP');
-    if (resp.ok) {
-      block.innerHTML = await resp.text();
+    const fragment = await loadFragment(xfPath);
+    console.log('Fetching XF');
+    if (fragment) {
+      // Move all content from the fragment to the block
+      block.replaceChildren(...fragment.childNodes);
     } else {
       block.innerHTML = '<p>Footer content not available</p>';
     }
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.error('Error loading royal footer:', error);
     block.innerHTML = '<p>Error loading footer content</p>';
   }
