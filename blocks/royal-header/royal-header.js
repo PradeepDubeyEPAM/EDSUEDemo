f41b1668be9c23043ec6281c26c82912698a848b
Sinchana Mruthyunjaya <sinchana_mruthyunjaya@epam.com>
2026-06-16 14:28:32 +0530

    logic update to read xf

--- blocks/royal-header/royal-header.js
+++ blocks/royal-header/royal-header.js
@@ -3,30 +3,26 @@ import { loadFragment } from '../fragment/fragment.js';
 const HEADER_FRAGMENT_PATH = '/content/experience-fragments/aem-cloud-poc/us/en/site/global/header/master';
 
 function renderFragment(block, fragment) {
-  const fragmentSection = fragment.querySelector(':scope .section');
-  if (fragmentSection && fragment.children.length === 1) {
+  const fragmentSection = fragment.querySelector(':scope > .section');
+  if (fragmentSection) {
     block.classList.add(...fragmentSection.classList);
     block.classList.remove('section');
     block.replaceChildren(...fragmentSection.childNodes);
     return;
   }
 
-  const content = document.createElement('div');
-  content.className = 'royal-header-content';
-  while (fragment.firstElementChild) {
-    content.append(fragment.firstElementChild);
-  }
-
-  block.append(content);
+  block.replaceChildren(...fragment.childNodes);
 }
 
 export default async function decorate(block) {
   const fragment = await loadFragment(HEADER_FRAGMENT_PATH);
-  block.textContent = '';
 
   if (!fragment) {
+    // eslint-disable-next-line no-console
+    console.error('Royal header fragment could not be loaded.', HEADER_FRAGMENT_PATH);
     return;
   }
 
+  block.textContent = '';
   renderFragment(block, fragment);
 }
